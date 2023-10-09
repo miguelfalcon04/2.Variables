981621564b0b816440b86c53697234888eac8585
miguelfalcon04 <falconmiguel.2004@gmail.com>
2023-10-09 08:30:48 +0200

    commit

---
+++ Eje03.java
@@ -0,0 +1,14 @@
+/*Ejercicio 3
+Crea las variables nombre, direccion y telefono y asígnales los valores correspondientes. 
+Muestra los valores de esas variables por pantalla de tal forma que el
+resultado del programa sea el mismo que en el ejercicio 2.
+*/
+
+public class Eje03 {
+    public static void main(String[] args) {
+        String nombre = "Miguel Jesús Falcón Carvajal";
+        String dir = "Calle Eneas nº 20, Marbella";
+        String telf = "617 80 95 85";
+        System.out.printf("%s\n%s\n%s\n",nombre, dir, telf    );
+    }
+}
