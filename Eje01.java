d383e66906cb6b60f9e9eb41352a238353138a9e
miguelfalcon04 <falconmiguel.2004@gmail.com>
2023-10-09 08:30:32 +0200

    commit

---
+++ Eje01.java
@@ -0,0 +1,18 @@
+/*Ejercicio 1
+Escribe un programa en el que se declaren las variables enteras x e y. Asígnales
+los valores 144 y 999 respectivamente. A continuación, muestra por pantalla
+el valor de cada variable, la suma, la resta, la división y la multiplicación.
+*/
+
+public class Eje01{
+    public static void main(String[] args) {
+        int x=144;
+        int y=999;
+
+        System.out.println("x= "+ x + " / y="+ y);
+        System.out.println("x+y= "+ (x+y));
+        System.out.println("x-y= "+ (x-y));
+        System.out.println("x/y= "+ ((float)x/(float)y));
+        System.out.println("x*y= "+ (x*y));
+    }
+}
